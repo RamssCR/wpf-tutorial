77f719e9e2016912c8e60ece18bb42c6a68d520f
RamssCR <ramsesgpm@gmail.com>
2025-06-25 16:33:01 -0500

    feat: layout

--- Tutorial/MainWindow.xaml.cs
+++ Tutorial/MainWindow.xaml.cs
@@ -7,23 +7,8 @@ namespace Tutorial;
 /// </summary>
 public partial class MainWindow : Window
 {
-    private bool _toggle = true;
-    
     public MainWindow()
     {
         InitializeComponent();
-    }
-
-    private void Button_OnClick(object sender, RoutedEventArgs e)
-    {
-        if (!_toggle)
-        {
-            Button.Content = "Okay, stop now.";
-        }
-        else
-        {
-            Button.Content = "HAHA, Nerd!!";
-            _toggle = false;
-        }
     }
 }
